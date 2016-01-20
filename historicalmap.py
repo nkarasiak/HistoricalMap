0e6386e02db2d28a58a0c9bca6cef864769c86ef
Nicart <nicart@gmail.com>
2016-01-20 08:55:13 +0100

    add time control

--- historicalmap.py
+++ historicalmap.py
@@ -6,7 +6,7 @@
 import dataraster
 import scipy as sp
 from scipy import ndimage
-
+import time
 
 """
   Filter class to isolate the forest and delete dark lines/fonts above.
@@ -88,7 +88,10 @@ class historicalFilter:
 if __name__=='__main__':
     # historicalFilter class
     folder="data/"
+    t1=time.clock()
     filtering=historicalFilter(folder+'map.tif',folder+'filtered',11,11)
+    t2=time.clock()
+    print t2-t1
 
     
 
