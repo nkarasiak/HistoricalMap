c160ae55e863a8232f0b74a736f46016eef2a2e3
Nicart <nicart@gmail.com>
2016-01-21 10:13:26 +0100

    add roi and def from Mathieu Fauvel

--- dataraster.py
+++ dataraster.py
@@ -108,3 +108,282 @@ def write_data_band(outname,im,d,GeoTransform,Projection):
 #            out.WriteArray(im[:,:,i])
 #            out.FlushCache()
 #    dst_ds = None
+    
+def get_samples_from_roi(raster_name,roi_name):
+    '''
+    The function get the set of pixels given the thematic map. Both map should be of same size. Data is read per block.
+    Input:
+        raster_name: the name of the raster file, could be any file that GDAL can open
+        roi_name: the name of the thematic image: each pixel whose values is greater than 0 is returned
+    Output:
+        X: the sample matrix. A nXd matrix, where n is the number of referenced pixels and d is the number of variables. Each 
+            line of the matrix is a pixel.
+        Y: the label of the pixel
+    ''' 
+    
+    ## Open Raster
+    raster = gdal.Open(raster_name,gdal.GA_ReadOnly)
+    if raster is None:
+        print 'Impossible to open '+raster_name
+        exit()
+
+    ## Open ROI
+    roi = gdal.Open(roi_name,gdal.GA_ReadOnly)
+    if roi is None:
+        print 'Impossible to open '+roi_name
+        exit()
+
+    ## Some tests
+    if (raster.RasterXSize != roi.RasterXSize) or (raster.RasterYSize != roi.RasterYSize):
+        print 'Images should be of the same size'
+        exit()
+
+    ## Get block size
+    band = raster.GetRasterBand(1)
+    block_sizes = band.GetBlockSize()
+    x_block_size = block_sizes[0]
+    y_block_size = block_sizes[1]
+    del band
+    
+    ## Get the number of variables and the size of the images
+    d  = raster.RasterCount
+    nc = raster.RasterXSize
+    nl = raster.RasterYSize
+
+    ## Read block data
+    X = sp.array([]).reshape(0,d)
+    Y = sp.array([]).reshape(0,1)
+    for i in range(0,nl,y_block_size):
+        if i + y_block_size < nl: # Check for size consistency in Y
+            lines = y_block_size
+        else:
+            lines = nl - i
+        for j in range(0,nc,x_block_size): # Check for size consistency in X
+            if j + x_block_size < nc:
+                cols = x_block_size
+            else:
+                cols = nc - j
+
+            # Load the reference data
+            ROI = roi.GetRasterBand(1).ReadAsArray(j, i, cols, lines)
+            t = sp.nonzero(ROI)
+            if t[0].size > 0:
+                Y = sp.concatenate((Y,ROI[t].reshape((t[0].shape[0],1)).astype('uint8')))
+                # Load the Variables
+                Xtp = sp.empty((t[0].shape[0],d))
+                for k in xrange(d):
+                    band = raster.GetRasterBand(k+1).ReadAsArray(j, i, cols, lines)
+                    Xtp[:,k] = band[t]
+                try:
+                    X = sp.concatenate((X,Xtp))
+                except MemoryError:
+                    print 'Impossible to allocate memory: ROI too big'
+                    exit()
+    
+    # Clean/Close variables
+    del Xtp,band    
+    roi = None # Close the roi file
+    raster = None # Close the raster file
+
+    return X,Y
+
+def predict_image(raster_name,classif_name,classifier,mask_name=None):
+    '''
+        The function classify the whole raster image, using per block image analysis. The classifier is given in classifier and options in kwargs
+    '''
+    # Parameters
+    block_sizes = 512
+
+    # Open Raster and get additionnal information
+    raster = gdal.Open(raster_name,gdal.GA_ReadOnly)
+    if raster is None:
+        print 'Impossible to open '+raster_name
+        exit()
+    
+    # If provided, open mask
+    if mask_name is None:
+        mask=None
+    else:
+        mask = gdal.Open(mask_name,gdal.GA_ReadOnly)
+        if mask is None:
+            print 'Impossible to open '+mask_name
+            exit()
+        # Check size
+        if (raster.RasterXSize != mask.RasterXSize) or (raster.RasterYSize != mask.RasterYSize):
+            print 'Image and mask should be of the same size'
+            exit()   
+        
+    # Get the size of the image
+    d  = raster.RasterCount
+    nc = raster.RasterXSize
+    nl = raster.RasterYSize
+    
+    # Get the geoinformation    
+    GeoTransform = raster.GetGeoTransform()
+    Projection = raster.GetProjection()
+    
+    # Set the block size 
+    x_block_size = block_sizes  
+    y_block_size = block_sizes
+    
+    ## Initialize the output
+    driver = gdal.GetDriverByName('GTiff')
+    dst_ds = driver.Create(classif_name, nc,nl, 1, gdal.GDT_UInt16)
+    dst_ds.SetGeoTransform(GeoTransform)
+    dst_ds.SetProjection(Projection)
+    out = dst_ds.GetRasterBand(1)
+    
+    ## Set the classifiers
+    if classifier['name'] is 'NPFS':
+        ## With GMM
+        model = classifier['model']
+        ids = classifier['ids']
+        nv = len(ids)
+    elif classifier['name'] is 'GMM':
+        model = classifier['model']   
+    
+    ## Perform the classification
+    for i in range(0,nl,y_block_size):
+        if i + y_block_size < nl: # Check for size consistency in Y
+            lines = y_block_size
+        else:
+            lines = nl - i
+        for j in range(0,nc,x_block_size): # Check for size consistency in X
+            if j + x_block_size < nc:
+                cols = x_block_size
+            else:
+                cols = nc - j
+                       
+            # Do the prediction
+            if classifier['name'] is 'NPFS':
+                # Load the data
+                X = sp.empty((cols*lines,nv))
+                for ind,v in enumerate(ids):
+                    X[:,ind] = raster.GetRasterBand(int(v+1)).ReadAsArray(j, i, cols, lines).reshape(cols*lines)
+                
+                # Do the prediction
+                if mask is None:
+                    yp = model.predict_gmm(X)[0].astype('uint16')
+                else:
+                    mask_temp=mask.GetRasterBand(1).ReadAsArray(j, i, cols, lines).reshape(cols*lines)
+                    t = sp.where(mask_temp!=0)[0]
+                    yp=sp.zeros((cols*lines,))
+                    yp[t]= model.predict_gmm(X[t,:])[0].astype('uint16')
+                    
+            elif classifier['name'] is 'GMM':
+                # Load the data
+                X = sp.empty((cols*lines,d))
+                for ind in xrange(d):
+                    X[:,ind] = raster.GetRasterBand(int(ind+1)).ReadAsArray(j, i, cols, lines).reshape(cols*lines)
+                
+                # Do the prediction
+                if mask is None:
+                    yp = model.predict_gmm(X)[0].astype('uint16')
+                else:
+                    mask_temp=mask.GetRasterBand(1).ReadAsArray(j, i, cols, lines).reshape(cols*lines)
+                    t = sp.where(mask_temp!=0)[0]
+                    yp=sp.zeros((cols*lines,))
+                    yp[t]= model.predict_gmm(X[t,:])[0].astype('uint16')
+                
+            # Write the data
+            out.WriteArray(yp.reshape(lines,cols),j,i)
+            out.FlushCache()
+            del X,yp
+
+    # Clean/Close variables    
+    raster = None
+    dst_ds = None
+
+def smooth_image(raster_name,mask_name,output_name,l,t):
+    '''
+    The function will apply a smoothing filter on all the pixels of the input image.
+    Input:
+    raster_name: the name of the originale SITS
+    mask_name: the name of the mask. In that file, every pixel with value greater than 0 is masked.
+    output_name: the name of the smoothed image
+    
+    TO DO: 
+    - check the input file format (uint16 or float)
+    - parallelization
+    '''
+    # Get 
+    import smoother as sm
+    # Open Raster and get additionnal information
+    raster = gdal.Open(raster_name,gdal.GA_ReadOnly)
+    if raster is None:
+        print 'Impossible to open '+raster_name
+        exit()
+
+    # Open Mask and get additionnal information
+    mask = gdal.Open(mask_name,gdal.GA_ReadOnly)
+    if raster is None:
+        print 'Impossible to open '+mask_name
+        exit()
+
+    # Check size
+    if (raster.RasterXSize != mask.RasterXSize) or (raster.RasterYSize != mask.RasterYSize) or (raster.RasterCount != mask.RasterCount):
+        print 'Image and mask should be of the same size'
+        exit() 
+    
+    # Get the size of the image
+    d  = raster.RasterCount
+    nc = raster.RasterXSize
+    nl = raster.RasterYSize
+
+    # Get the geoinformation    
+    GeoTransform = raster.GetGeoTransform()
+    Projection = raster.GetProjection()
+
+    # Get block size
+    band = raster.GetRasterBand(1)
+    block_sizes = band.GetBlockSize()
+    x_block_size = block_sizes[0]
+    y_block_size = block_sizes[1]
+    del band
+
+    # Initialize the output
+    driver = gdal.GetDriverByName('GTiff')
+    dst_ds = driver.Create(output_name, nc,nl, d, gdal.GDT_Float64)
+    dst_ds.SetGeoTransform(GeoTransform)
+    dst_ds.SetProjection(Projection)
+
+    for i in xrange(0,nl,y_block_size):
+        if i + y_block_size < nl: # Check for size consistency in Y
+            lines = y_block_size
+        else:
+            lines = nl - i
+        for j in xrange(0,nc,x_block_size): # Check for size consistency in X
+            if j + x_block_size < nc:
+                cols = x_block_size
+            else:
+                cols = nc - j
+
+            # Get the data
+            X = sp.empty((cols*lines,d))
+            M = sp.empty((cols*lines,d),dtype='int')
+            for ind in xrange(d):
+                X[:,ind] = raster.GetRasterBand(int(ind+1)).ReadAsArray(j, i, cols, lines).reshape(cols*lines)
+                M[:,ind] = mask.GetRasterBand(int(ind+1)).ReadAsArray(j, i, cols, lines).reshape(cols*lines)
+            # Put all masked value to 1
+            M[M>0]=1
+            
+            # Do the smoothing
+            Xf = sp.empty((cols*lines,d))
+            for ind in xrange(cols*lines): # This part can be speed up by doint it in parallel
+                smoother = sm.Whittaker(x=X[ind,:],t=t,w=1-M[ind,:],order=2)
+                Xf[ind,:] = smoother.smooth(l)
+
+            # Write the data
+            for ind in xrange(d):
+                out = dst_ds.GetRasterBand(ind+1)
+                out.WriteArray(Xf[:,ind].reshape(lines,cols),j,i)
+                out.FlushCache()
+
+            # Free memory
+            del X,Xf,M,out
+
+    # Clean/Close variables
+    raster = None
+    mask = None
+    dst_ds = None
+
